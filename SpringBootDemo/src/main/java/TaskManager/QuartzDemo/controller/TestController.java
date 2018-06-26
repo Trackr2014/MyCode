3f568962cae463af3f9f1649ee6436e90fbabfb2
Tracker <1150680039@qq.com>
2018-06-26 16:38:16 +0800

    “集成了redis以及log4j2”

--- SpringBootDemo/src/main/java/TaskManager/QuartzDemo/controller/TestController.java
+++ SpringBootDemo/src/main/java/TaskManager/QuartzDemo/controller/TestController.java
@@ -4,18 +4,33 @@
  */
 package TaskManager.QuartzDemo.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 @RestController
 public class TestController {
-
+	private static final Logger logger = LogManager.getLogger(TestController.class);
+    Jedis jsJedis;
+	JedisPool jedisPool;
 	@RequestMapping("/helloworld")
     public String helloworld() {
-        return "helloworld";
+		logger.info("你好 hello日志");
+		jedisPool = new JedisPool(new JedisPoolConfig(), "127.0.0.1");
+		jsJedis = jedisPool.getResource();
+		jsJedis.auth("123456");
+		jsJedis.set("001", "helloJedis");
+		logger.error("错误");
+	    return jsJedis.get("001");
     }	
 	@RequestMapping("/springBoot")
 	public String springBoot() {
+		logger.info("执行了springBoot");
 		return "this a demo for springBoot";		
 	}
 }
