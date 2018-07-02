5c77382857c709670964d87564840548f73d515c
Tracker <1150680039@qq.com>
2018-07-02 15:27:31 +0800

    “redis配置”

--- SpringBootDemo/src/main/java/TaskManager/QuartzDemo/controller/TestController.java
+++ SpringBootDemo/src/main/java/TaskManager/QuartzDemo/controller/TestController.java
@@ -6,32 +6,32 @@ package TaskManager.QuartzDemo.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
+import Util.RedisUtil;
 
 @RestController
 public class TestController {
 	private static final Logger logger = LogManager.getLogger(TestController.class);
-    Jedis jsJedis;
-	JedisPool jedisPool;
 	@RequestMapping("/helloworld")
     public String helloworld() {
 		for(int i=0; i<50000; i++){
 			logger.info("你好 hello日志");
-//			jedisPool = new JedisPool(new JedisPoolConfig(), "127.0.0.1");
-//			jsJedis = jedisPool.getResource();
-//			jsJedis.auth("123456");
-//			jsJedis.set("001", "helloJedis");
 			logger.error("错误");
 		}
 	    return "hello";
     }	
 	@RequestMapping("/springBoot")
 	public String springBoot() {
+		@SuppressWarnings("resource")
+		ApplicationContext context=new ClassPathXmlApplicationContext("classpath:applicationContext-redis.xml");
+		RedisUtil redisUtil=(RedisUtil) context.getBean("redisUtil");
+		redisUtil.set("001", "wang.pengfei");
+		redisUtil.get("001");
+		System.out.println(redisUtil.get("001"));
 		logger.info("执行了springBoot");
 		return "this a demo for springBoot";		
 	}
