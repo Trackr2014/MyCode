2be212eff34fa18b26c9b5c48460a0ba2ac0b2a9
Tracker <1150680039@qq.com>
2019-04-09 11:45:16 +0800

    代码提交

--- SpringRabbitMQDemo/src/main/java/com/example/demo/config/RabbitMQTemplate.java
+++ SpringRabbitMQDemo/src/main/java/com/example/demo/config/RabbitMQTemplate.java
@@ -2,36 +2,28 @@ package com.example.demo.config;
 
 
 
-import org.springframework.amqp.core.AcknowledgeMode;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 
 @Configuration
-public class RabbitMQTemplate implements RabbitTemplate.ConfirmCallback{
+public class RabbitMQTemplate implements RabbitTemplate.ConfirmCallback, RabbitTemplate.ReturnCallback{
 	
 	@Autowired
 	ConnectionFactory connectionFactory;
 	
-	@Bean
-	public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(ConnectionFactory connectionFactory){
-	    SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-	    factory.setConnectionFactory(connectionFactory);
-	    factory.setMessageConverter(new Jackson2JsonMessageConverter());
-	    factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);             //开启手动 ack
-	    return factory;
-	}
-	
     @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public RabbitTemplate rabbitTemplate() {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setConfirmCallback(this);
+        rabbitTemplate.setReturnCallback(this);
         return rabbitTemplate;
     }
 
@@ -43,6 +35,10 @@ public class RabbitMQTemplate implements RabbitTemplate.ConfirmCallback{
 			System.out.println("消息发送到Exchange失败！");
 		}
 	}
-	
 
+	@Override
+	public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+		System.out.println("return--message:" + new String(message.getBody()) + ",replyCode:" + replyCode
+				+ ",replyText:" + replyText + ",exchange:" + exchange + ",routingKey:" + routingKey);
+	}
 }
