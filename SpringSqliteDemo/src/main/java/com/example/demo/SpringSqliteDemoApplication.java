148e076ba626d56ebdd4e6cad5552dc6945fb394
Tracker <1150680039@qq.com>
2019-08-13 16:06:04 +0800

    配置https

--- SpringSqliteDemo/src/main/java/com/example/demo/SpringSqliteDemoApplication.java
+++ SpringSqliteDemo/src/main/java/com/example/demo/SpringSqliteDemoApplication.java
@@ -1,12 +1,57 @@
 package com.example.demo;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ServletComponentScan
 public class SpringSqliteDemoApplication {
+
+	// 如果没有使用默认值8080
+	@Value("${http.port}")
+	Integer httpPort;
+	// 正常启用的https端口 如443
+	@Value("${server.port}")
+	Integer httpsPort;
+
+	// springboot2 写法
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
+        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint=new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection=new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
+    }
+
+    @Bean
+	public Connector httpConnector() {
+		System.out.println("启用http转https协议，http端口：" + this.httpPort + "，https端口：" + this.httpsPort);
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		// Connector监听的http的端口号
+		connector.setPort(httpPort);
+		connector.setSecure(false);
+		// 监听到http的端口号后转向到的https的端口号
+		connector.setRedirectPort(httpsPort);
+		return connector;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSqliteDemoApplication.class, args);
