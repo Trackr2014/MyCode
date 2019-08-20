44cdb5a2bf7a4d47ae659b75b0be6e5c9ec129e9
Tracker <1150680039@qq.com>
2019-08-20 17:52:00 +0800

    增加双重校验模式单例例子

--- SpringSqliteDemo/src/main/java/com/example/demo/controller/PropertiesValueController.java
+++ SpringSqliteDemo/src/main/java/com/example/demo/controller/PropertiesValueController.java
@@ -7,26 +7,42 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.bean.PropertiesValueBean;
+import com.example.demo.singleCase.ApplicationConfig;
 
 @RestController
 @RequestMapping(value = "/v1/properties")
 public class PropertiesValueController {
-	
+
 	@Autowired
 	PropertiesValueBean propertiesValueBean;
-	
-	@Value("${server.port}")
-	private String serverPort;
-	
-	@RequestMapping(value = "/bean",method = RequestMethod.GET)
+
+	@Value("${http.port}")
+	private String httpPort;
+
+	@RequestMapping(value = "/bean", method = RequestMethod.GET)
 	public PropertiesValueBean getProperties() {
-		return propertiesValueBean;	
+		return propertiesValueBean;
 	}
-	
+
+	@RequestMapping(value = "/port", method = RequestMethod.GET)
+	public String getPortProperties() {
+		return httpPort;
+	}
+
 	@RequestMapping(value = "/string", method = RequestMethod.GET)
 	public String getPropertyString() {
-		return serverPort;
+		for (int i = 0; i < 10; i++) {
+			new Thread() {
+				public void run() {
+					ApplicationConfig config = ApplicationConfig.getInstance();
+					System.out.println("=====" + config);
+					System.out.println(config.getConfiguration()+ "======");
+				}
+
+			}.start();
+			System.out.println("i======" + i);
+		}
+		return httpPort;
 	}
-	
-	
+
 }
